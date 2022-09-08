b92b73aa76ca382b09ccc5b72179550bfe59618c
lawalafeez820 <lawalafeez820@gmail.com>
2022-09-08 08:13:55 -0700

    sizeof memory

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,25 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	char i;
+
+	int k;
+
+	long int j;
+
+	float f;
+
+	long long int l;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(i));
+
+	printf("Size of an int: %lu byte(s)\n", sizeof(k));
+
+	printf("Size of a long int: %lu byte(s)\n", sizeof(j));
+
+	printf("Size of an long long int: %lu byte(s)\n", sizeof(l));
+
+	printf("Size of an float: %lu byte(s)\n", sizeof(f));
 
 	return (0);
 }
