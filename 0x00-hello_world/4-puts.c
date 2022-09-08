035393a8fa4a0c931a4a327edff7effb9c4d021c
lawalafeez820 <lawalafeez820@gmail.com>
2022-09-08 06:54:34 -0700

    using puts for printing

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * Program to print exactly "Programming is like building a multilingual puzzle  * using puts
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
