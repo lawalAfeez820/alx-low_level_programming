b9cd4e956fdbc8d54b18f169f5a5748013f2b4ee
lawalafeez820 <lawalafeez820@gmail.com>
2022-09-08 12:41:17 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - program to show memory size
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-    char i;
-    int j;
-    long int k;
-    long long int l;
-    float m;
-    
-    printf("Size of a char: %lu byte(s)\n", sizeof(i));
-    printf("Size of a int: %lu byte(s)\n", sizeof(j));
-    printf("Size of a long int: %lu byte(s)\n", sizeof(k));
-    printf("Size of a long long int: %lu byte(s)\n", sizeof(l));
-    printf("Size of a float: %lu byte(s)\n", sizeof(m));
-    return (0);
+	char i;
+	int j;
+	long int k;
+	long long int l;
+	float m;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(i));
+	printf("Size of an int: %lu byte(s)\n", sizeof(j));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(k));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(l));
+	printf("Size of a float: %lu byte(s)\n", sizeof(m));
+	return (0);
 }
