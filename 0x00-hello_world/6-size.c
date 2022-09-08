50584cc49735a364c6fb04c52913dacd689e89e6
lawalafeez820 <lawalafeez820@gmail.com>
2022-09-08 08:17:11 -0700

    sizeof memory

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,22 +6,26 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{ 
+{
 	char i;
-	int j;
-	long int k;
-	long long int l;
+
+	int k;
+
+	long int j;
+
 	float f;
+
+	long long int l;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(i));
 
-	printf("Size of an int: %lu byte(s)\n", sizeof(j));
+	printf("Size of an int: %lu byte(s)\n", sizeof(k));
 
-	printf("Size of a long int: %lu byte(s)\n", sizeof(k));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(j));
 
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(l));
+	printf("Size of an long long int: %lu byte(s)\n", sizeof(l));
 
-	printf("Size of a float: %lu byte(s)\n", sizeof(f));
+	printf("Size of an float: %lu byte(s)\n", sizeof(f));
 
 	return (0);
 }
