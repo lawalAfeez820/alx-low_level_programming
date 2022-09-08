02e4bf325a7ebd1ba39722ea7e8c1cc1c56448c3
lawalafeez820 <lawalafeez820@gmail.com>
2022-09-08 12:08:40 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,5 +19,5 @@ int main(void)
     printf("Size of a long int: %lu byte(s)\n", sizeof(k));
     printf("Size of a long long int: %lu byte(s)\n", sizeof(l));
     printf("Size of a float: %lu byte(s)\n", sizeof(m));
-    Return (0);
+    return (0);
 }
