9e0585c0e2dda8826a666cc4ea93db0a9a2aef23
lawalafeez820 <lawalafeez820@gmail.com>
2022-09-08 07:42:31 -0700

    sizeof datatype

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{ 
+	char i;
+	int j;
+	long int k;
+	long long int l;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(i));
+	printf("Size of an int: %lu byte(s)\n", sizeof(j));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(k));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(l));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
+	return (0);
+}
