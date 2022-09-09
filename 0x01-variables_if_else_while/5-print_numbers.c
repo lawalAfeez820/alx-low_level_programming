693aae517733038e5eadf329e98fb9f3f78fe377
lawalafeez820 <lawalafeez820@gmail.com>
2022-09-09 09:24:59 -0700

    base10 number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - printing number from 0 to 9
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
