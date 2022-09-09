d2f7fbfaeb14b9a238d36ca84dd23d9ce811f5e0
lawalAfeez <lawalAfeez820@gmail.com>
2022-09-09 12:03:03 -0700

    lower and upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - upper and lower
+ *
+ * Returns: always 0
+ */
+int main(void)
+{
+	char uplo[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+		putchar(uplo[i]);
+	putchar('\n');
+	return (0);
+}
