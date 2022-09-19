e2084f3705732d1b4550f051f3981c4552346899
lawalafeez820 <lawalafeez820@gmail.com>
2022-09-19 11:21:30 -0700

    converting string to int and token generation added

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,92 +1,42 @@
+#include <stdlib.h>
 #include "main.h"
-int _pow(int a, int b);
-int _strlen(char *s);
+#include <limits.h>
+#include <string.h>
 
 /**
- * _atoi: convert sting to int
- * @s: string to be converted
- *Return: integer output
+ * _atoi - coverts a string to integer
+ * @str: pointer to string to be converted
+ *
+ * Return: converted string
+ *
+ * Created by: Lawal Afeez
+ * cc: 19th Sept, 2022.
  */
-int _atoi(char *s)
+
+int _atoi(char *str)
 {
-    int i;
-    int k;
-    int final = 0;
-    int count = 0;
-    int track = 0;
-    int total = 0;
-    int first;
-    int track2;
+	int sign = 1, base = 0, i = 0;
 
-    if (_strlen(s) > 0)
-    {
-    
-    for (i = 0; s[i]; i++)
-    {
-        if (s[i] >= 48 && s[i] <= 57)
-            {
-                count = i;
-                track = track + 1;
-            }
-        if ( i > count && track > 0)
-            break;
-    }
-    if (i > count)
-    {
-        first = i - track;
-    }
-    else if (i == count)
-    {
-        first = i - track + 1;
-    }
-    final = count;
-    track2 = track;
-    track = track - 1;
-    for (k = first; k <= final; k++)
-    {
-        total = total + ((s[k] - 48) * _pow(10,track));
-        track = track - 1;
-    }
-    if (track2 + 1 == _strlen(s))
-        return (-1 * total);
-    else if (count == i && s[i - track2] == '-')
-        return (-1 * total);
-    else if (count == i && s[i - track2] == '+')
-        return (-1 * total);
-    else
-        return (total);
-    }
-    else
-        return (0);
+	for (i = 0; str[i] != '\0' && (str[i] < '0' || str[i] > '9'); i++)
+	{
+		if (str[i] == '-' || str[i] == '+')
+			sign *= 1 - 2 * (str[i] == '-');
+		if (str[i + 1] == '\0')
+			return (0);
+	}
+
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		if (base > INT_MAX / 10	|| (base == INT_MAX / 10
+			&& str[i] - '0' > 7))
+		{
+			if (sign == 1)
+				return (INT_MAX);
+			else
+				return (INT_MIN);
+		}
+		base = 10 * base + (str[i++] - '0');
+	}
+
+	return (base * sign);
 }
-
-int _strlen(char *s)
-{
-    int i;
-    int count = 0;
-    
-    for (i = 0; s[i]; i++)
-        count = count + 1;
-    return (count);
-}
-
-int _pow(int a, int b)
-{
-    int i;
-    int sum = a;
-    
-    if (b == 0)
-        return (1);
-    else if (b == 1)
-        return (a);
-    else
-    {
-        for (i = 1; i < b ; i++)
-        {
-                sum = sum * a;
-        }
-        return (sum);
-    }
-}
-
-
