30b8579b1aa3c00a9632a1f9c1d44f63ce47a981
lawalafeez820 <lawalafeez820@gmail.com>
2022-09-19 01:22:23 -0700

    reversing string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main"
+
+/**
+ * print_rev - reverse a string
+ *
+ * @s: sting to be reverved
+ *
+ * Return: none
+ */
+void print_rev(char *s)
+{
+	int i, n;
+	int count = 0;
+
+	for (i = _strlen(s) - n, n = 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
