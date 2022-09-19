5e394436ba1b6a225a8f46d99a754e822544c81c
lawalafeez820 <lawalafeez820@gmail.com>
2022-09-19 02:05:43 -0700

    reversing strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+int _strlen(char *s);
 
 /**
  * print_rev - reverse a string
@@ -16,4 +17,20 @@ void print_rev(char *s)
 	for (i = n; i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
+}
+
+/**
+ * _strlen - strlength
+ * @s: string to find it length
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; *(s + 1); i++)
+		count++;
+	return (count);
 }
