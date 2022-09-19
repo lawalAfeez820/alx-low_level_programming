92dca6d35da56835d3cce4d404f185d12876e0b7
lawalafeez820 <lawalafeez820@gmail.com>
2022-09-19 00:39:08 -0700

    swapping integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - change the position of two numbers
+ * 
+ * @a: pointer variable of the first number
+ * @b: pointer variable of the second number
+ * Author: Lawal Afeez
+ * Return: none
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
