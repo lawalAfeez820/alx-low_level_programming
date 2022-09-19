7d37dcaf4861720a1eb79fd3641d239fe6b21f53
lawalafeez820 <lawalafeez820@gmail.com>
2022-09-19 00:32:12 -0700

    reset a value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset a number to 98
+ *
+ * @n: variable to reset
+ * Author: Lawal Afeez
+ *
+ * Return: none
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
