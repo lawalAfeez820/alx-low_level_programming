eb0831dd7bbb5f167e6804ca49c37db73af3b45b
lawalafeez820 <lawalafeez820@gmail.com>
2022-09-22 01:06:23 -0700

    mental math

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,81 +1,35 @@
-#include "main.h"
 #include <stdio.h>
-int _pow(int a, int b);
-int powof(int n);
-
+#include "main.h"
 /**
- *print_number - print numbers
- *
- * @n: number to be printed
- * Return: none
+ * print_number - prints a number
+ * @n: Input number
  */
+
 void print_number(int n)
 {
-	int i;
-	int k, j;
+	int res, temp, expo;
 
-	if (n == 0)
-		_putchar(0 + '0');
-
-	else if  (n < 0 || n > 0)
+	expo = 1;
+/*Check negatives*/
+	if (n >= 0)
+		res = n * -1;
+	else
 	{
-		if (n < 0)
-		{
-			n = -1 * n;
-			_putchar('-');
-		}
-		k = powof(n);
+		res = n;
+		_putchar('-');
+	}
 
-		for (i = k; i >= 0; i--)
-		{
-
-			j = n / _pow(10, i);
-			_putchar((j % 10) + '0');
-		}
+/*Initialize exponent variable*/
+	temp = res;
+	while (temp <= -10)
+	{
+		expo *= 10;
+		temp /= 10;
+	}
+/*Main */
+	while (expo >= 1)
+	{
+		_putchar(((res / expo) % 10) * -1 + '0');
+		expo /= 10;
 	}
 }
-
-/**
- * _pow - finding a raised to the power of b
- *
- * @a: the base
- * @b: the index
- *
- * Return: a raised to power b
- */
-int _pow(int a, int b)
-{
-	int sum, i;
-
-	if (b == 0)
-		return (1);
-	else if (b == 1)
-		return (a);
-	sum = a;
-	for (i = 1; i < b; i++)
-	{
-		sum = sum * a;
-	}
-	return (sum);
-}
-
-/**
- * powof - find the range in which a number falls
- *
- * @n: number to find
- * Return: int
- */
-int powof(int n)
-{
-	int i;
-
-	for (i = 0; i <= 10; i++)
-	{
-		if (n >= _pow(10, i) && n < _pow(10, i + 1))
-		{
-			return (i);
-		}
-	}
-	return (i);
-}
-
