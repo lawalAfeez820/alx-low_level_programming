2ffd4c0231a6d87c4d7b90646efd9f5d432d93c8
lawalafeez820 <lawalafeez820@gmail.com>
2022-09-21 23:47:17 -0700

    capitalizing

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,24 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * cap_string - capitalize strings
- *
- * @s: string to capitalize
- * Return: pointer to the string address
+ * cap_string - capitalizes all words of a string.
+ * @s: pointer to input string.
+ * Return: Returns pointer to capitalized string.
  */
 char *cap_string(char *s)
 {
-	char d[] = {'\n', '\t', ' ', ',', ';', '.', '!', '?', '"', '(', ')',
-		'{', '}'};
-	int len = strlen(d);
-	int len_s = strlen(s);
 	int i, j;
+	char sep[] = " \t\n,;.!?\"(){}";
 
-	for (i = 0; i < len_s; i++)
+	i = 1;
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= ('a' - 'A');
+	while (s[i] != '\0')
 	{
-
-		if (i == len_s - 1)
-			break;
-		for (j = 0; j < len; j++)
-		{
-			if (s[i] == d[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			{
-				s[i + 1] = s[i + 1] - 32;
-			}
-		}
+		for (j = 0; sep[j] != '\0'; j++)
+			if (s[i - 1] == sep[j] && (s[i] >= 'a' && s[i] <= 'z'))
+				s[i] -= ('a' - 'A');
+		i++;
 	}
 	return (s);
 }
-
-
