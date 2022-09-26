5c5999bb63cf7e99ace66f65779ecdb7398bc96f
lawalafeez820 <lawalafeez820@gmail.com>
2022-09-26 05:27:18 -0700

    strchr in c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - function finds the occurrence of the c
+ * in the  string s.
+ *  The terminating null bytes (\0) are not compared
+ *
+ * @s: string to search from
+ * @c:character to check match
+ * Return: pointer to address of the match
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *p = NULL;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (p);
+}
