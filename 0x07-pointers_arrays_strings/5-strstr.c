94d3dd21f769dc02d945e4fe30093c6cefb387aa
lawalafeez820 <lawalafeez820@gmail.com>
2022-09-26 13:43:18 -0700

    strttr in c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,26 +1,41 @@
-#include "main.h"
-#include <string.h>
-
+#include "main.h" 
 /**
- * _strstr - function finds the first occurrence of the substring
- *  needle in the  string haystack.
- *  The terminating null bytes (\0) are not compared
- *
- * @haystack: string to search from
- * @needle:string to check match
- * Return: pointer to address of the fisrt match
+ * _strncmp - compares two strings within b bytes.
+ * @s1: string to check
+ * @s2: string to check from
+ * @b: number of bytes to check
+ * Return: 0 if strings are the same
+ * any other number if they are different.
+ */
+int _strncmp(char *s1, char *s2, unsigned int b)
+{
+	unsigned int i;
+	for (i = 0; i <= b && *s1 == *s2 && *s1 != '\0' && *s2 != '\0'; i++)
+	{
+		s1++;
+		s2++;
+	}
+	return (i - b);
+}
+/**
+ * _strstr - locates a substring
+ * @haystack: string to look though
+ * @needle: substring to find
+ * Return: pointer to string with result of search
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
-
-	for (i = 0; needle[i]; i++)
+	unsigned int len;
+	/*Get length of needle for strncmp*/
+	len = 0;
+	while (needle[len] != '\0')
+		len++;
+	/*compare substring*/
+	while (*haystack != '\0')
 	{
-		for (j = 0; haystack[j]; j++)
-		{
-			if (needle[i] == haystack[j])
-				return (&haystack[j]);
-		}
+		if (_strncmp(haystack, needle, len) == 0)
+			return (haystack);
+		haystack++;
 	}
-	return (&haystack[strlen(haystack)]);
+	return (NULL);
 }
