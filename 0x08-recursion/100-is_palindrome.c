978c077888aca5e5c9cc70b81fcc9cf2c0bd621a
lawalafeez820 <lawalafeez820@gmail.com>
2022-09-28 02:41:34 -0700

    is palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "string.h"
+int is_pal(char *s, int n, int count, int j, int i);
 
 /**
  * is_palindrome - check if a string is palindrome
@@ -8,19 +9,33 @@
  *
  * Return: 1 if true else 0
  */
+
 int is_palindrome(char *s)
 {
 	int n = strlen(s);
-	int count = 0;
-	int j, i;
+	int j = n - 1;
 
-	for (i = 0, j = n - 1; i < n / 2; i++, j--)
-	{
-		if (*(s + i) == *(s + j))
-			count++;
-	}
-	if (count == n / 2)
+	return (is_pal(s, n, 0, j, 0));
+}
+
+/**
+ * is_pal - check if a string is palindrome
+ * @s: the string
+ * @n: length of the string
+ * @count: counter
+ * @i: front index
+ * @j: back index
+ *Return: 1 if true else zero
+ */
+int is_pal(char *s, int n, int count, int j, int i)
+{
+
+	if (count == n / 2 && i == n / 2)
 		return (1);
-	return (0);
+	if (i > n / 2)
+		return (0);
+	if (*(s + i) == *(s + j))
+		return (is_pal(s, n, count + 1, j - 1, i + 1));
+	return (is_pal(s, n, count, j - 1, i + 1));
 }
 
