521f86fab1df07a0d425dca1bda8383cb73fb826
lawalafeez820 <lawalafeez820@gmail.com>
2022-09-28 00:26:44 -0700

    pow of a number using recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - power of a numnber
+ *
+ * @x: the base
+ * @y: the power
+ * Return: the power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (_pow_recursion(x, y - 1) * x);
+}
