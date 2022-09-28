b076c193be422bedac293fdfdf1236ad0cbda0bf
lawalafeez820 <lawalafeez820@gmail.com>
2022-09-28 02:04:17 -0700

    check if number is prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+int is_prime(int n, int i, int count);
 
 /**
  * is_prime_number - check if a number is prime
@@ -7,23 +8,32 @@
  *
  * Return: 1 if prime else 0
  */
+
 int is_prime_number(int n)
 {
-	int count = 0;
-	int i;
-
-	if (n <= 1)
-		return (0);
-	for (i = 1; (i < n / 2 + 1); i++)
-	{
-		if (n % i == 0)
-		{
-			count++;
-			if (count > 1)
-				return (0);
-		}
-	}
-	if (count == 1)
-		return (1);
-	return (0);
+	return (is_prime(n, 1, 0));
 }
+
+/**
+ * is_prime - check if a number is prime
+ *
+ * @n: the number to be checked
+ * @i: it factors
+ * @count : counter
+ * Return: 1 if prime else 0
+ */
+int is_prime(int n, int i, int count)
+{
+	if (n < 2)
+		return (0);
+
+	if (i >= n / 2 + 1 && count == 1)
+		return (1);
+	if (i < n / 2 + 1 && count > 1)
+		return (0);
+	if (n % i == 0)
+		return (is_prime(n, i + 1, count + 1));
+	return (is_prime(n, i + 1, count));
+}
+
+
