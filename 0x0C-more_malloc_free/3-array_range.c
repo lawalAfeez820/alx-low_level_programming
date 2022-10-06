7962831cd5cc0de71701b00f6ba5162f5f9a831e
lawalafeez820 <lawalafeez820@gmail.com>
2022-10-06 05:31:19 -0700

    corrected

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,6 +16,8 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	p = malloc((max - min + 1) * sizeof(int));
+	if (p == NULL)
+		return (NULL);
 	for (i = min, j = 0; i <= max; i++, j++)
 		p[j] = i;
 	return (p);
