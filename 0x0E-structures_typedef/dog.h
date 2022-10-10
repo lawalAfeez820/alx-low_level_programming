96a4d4089e81ecee10a5cd30e3614da479adb927
lawalafeez820 <lawalafeez820@gmail.com>
2022-10-10 08:06:15 -0700

    initial commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,15 +2,22 @@
 #define DOG_H
 
 /**
- *struct dog - define a mew datatype dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: dog owner
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: age integer
+ * @owner: character string
  */
-struct dog
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
