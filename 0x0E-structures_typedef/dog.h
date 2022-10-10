fe30292008b53c2dd518f1aa6b1138c2176061b9
lawalafeez820 <lawalafeez820@gmail.com>
2022-10-10 07:04:11 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog_H
+#define dog_H
+
+/**
+ *struct dog - define a mew datatype dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
