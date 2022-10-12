5074d5d24e76ed64e29c6599595dec4f6ab0b994
lawalafeez820 <lawalafeez820@gmail.com>
2022-10-12 09:07:08 -0700

    function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,15 @@
 #include "function_pointers.h"
 
 /**
- * print_name -print name
- * @f: pointer to a function
- * @name: name to print
- * Return: none
+ * print_name - print a name using function call back
+ * @name: pointer to the name to be printed
+ * @f: function pointer to call back a function
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
