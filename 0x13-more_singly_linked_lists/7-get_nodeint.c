ad81e405cbe7ad0d01bb9352963161fdbca7658f
lawalafeez820 <lawalafeez820@gmail.com>
2022-10-24 16:05:25 -0700

    node searching

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - get the node at a certai *n index
+ *
+ * @head: poiter to the starting
+ * @index: index to search
+ * Return: poiter to d node searched
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head)
+	{
+		head = head->next;
+		i++;
+		if (i == index + 1)
+			return (head);
+	}
+	return (NULL);
+}
