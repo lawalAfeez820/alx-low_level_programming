f078d39ccd4445d75116bbff4ae86f1dbccfea92
lawalafeez820 <lawalafeez820@gmail.com>
2022-10-27 08:44:02 -0700

    decimal to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,60 +1,19 @@
-#include "main.h"
-int _find(unsigned long int n, int i);
-unsigned long int _mul(int i);
-
+#include "holberton.h"
 /**
- * print_binary - print binary of a number
- * @n: number to print it binary
- * Return: none/
- */
+  * print_binary - converts a decimal number to binary
+  * @n: integer to convert
+  */
 void print_binary(unsigned long int n)
 {
-	int x, j;
+	int i, flag;
 
 	if (n == 0)
+		_putchar('0');
+	for (flag = 0, i = sizeof(n) * 8 - 1; i >= 0; i--)
 	{
-		_putchar(0 + '0');
-		return;
+		if ((n >> i) & 1)
+		flag = 1;
+		if (flag == 1)
+			((n >> i) & 1) ? _putchar('1') : _putchar('0');
 	}
-	if (n < 2)
-	{
-		_putchar(1 + '0');
-		return;
-	}
-
-	x = _find(n, 0);
-	j = n - _mul(x);
-	print_binary(x);
-	_putchar(j + '0');
-
-}
-
-/**
- * _find - find where the multiplication of a numb * er falls
- * @n: the number
- * @i: where it falls
- * Return: i
- */
-
-int _find(unsigned long int n, int i)
-{
-	if (n == 0)
-		return (0);
-
-	if (_mul(i) <= n && _mul(i + 1) > n)
-		return (i);
-	return (_find(n, i + 1));
-}
-
-
-/**
- * _mul - multiplication table of 2
- *@i: value
- * Return: int
- */
-unsigned long int _mul(int i)
-{
-	if (i == 0)
-		return (0);
-	return (2 + _mul(i - 1));
 }
